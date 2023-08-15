0f7baf2d1a386e7fca07aecc4db8c25824f1e75c
Claudiu Sonica <csonica95@gmail.com>
2023-08-15 19:37:25 +0200

    completed data object for components

--- src/data.js
+++ src/data.js
@@ -4,28 +4,32 @@ const data = {
       {
         id: 1,
         title: "The Best Dark Academia Books",
-        description: "Abounding in sinister secrets - sometimes magical, sometimes not - locked in the libraries and halls of mysterious educational institutions, dark academia has become a phenomenally successful literary genre...",
-        img: "/assets/images/news-slider/photo-news3.png"
+        description:
+          "Abounding in sinister secrets - sometimes magical, sometimes not - locked in the libraries and halls of mysterious educational institutions, dark academia has become a phenomenally successful literary genre...",
+        img: "/assets/images/news-slider/photo-news3.png",
       },
       {
         id: 2,
         title: "The Books You Need to Read in 2023",
-        description: "This is the blog we know you've all been waiting for. We present the top 10 titles for 2023 in fiction, non-fiction and children's books; a glorious mix of masterful storytelling, compelling subject matter and...",
-        img: "/assets/images/news-slider/photo-news1.png"
+        description:
+          "This is the blog we know you've all been waiting for. We present the top 10 titles for 2023 in fiction, non-fiction and children's books; a glorious mix of masterful storytelling, compelling subject matter and...",
+        img: "/assets/images/news-slider/photo-news1.png",
       },
       {
         id: 3,
         title: "February's Best Children's Books",
-        description: "Some of the finest children's authors currently writing have books publishing this month, from Natasha Farrant to Elle McNicoll and from Tahereh Mafi to Harriet Muncaster. Across all areas and age ranges...",
-        img: "/assets/images/news-slider/photo-news2.png"
+        description:
+          "Some of the finest children's authors currently writing have books publishing this month, from Natasha Farrant to Elle McNicoll and from Tahereh Mafi to Harriet Muncaster. Across all areas and age ranges...",
+        img: "/assets/images/news-slider/photo-news2.png",
       },
       {
         id: 4,
         title: "What to Read Next After Damascus Station",
-        description: "A spy thriller with real authenticity due to its author's previous career as a CIA operative, Damascus Station has proven to be a phenomenal word-of-mouth hit since its publication in January. With its riveting story of... ",
-        img: "assets/images/news-slider/photo-news4.png"
-      }
-    ]
+        description:
+          "A spy thriller with real authenticity due to its author's previous career as a CIA operative, Damascus Station has proven to be a phenomenal word-of-mouth hit since its publication in January. With its riveting story of... ",
+        img: "assets/images/news-slider/photo-news4.png",
+      },
+    ],
   },
   top: {
     title: "Top Sellers",
@@ -33,18 +37,87 @@ const data = {
       {
         id: 1,
         title: "The Priory of the Orange...",
-        description: "The House of Berethnet has ruled Inys for a thousand years. Still unwed, Queen Sabran...",
-        img: "/assets/images/top-slider/book7.png",
+        description:
+          "The House of Berethnet has ruled Inys for a thousand years. Still unwed, Queen Sabran...",
+        img: "/assets/images/top-slider/top2.png",
         price: "$19.99",
+        cart: "/assets/images/icon.svg",
+        wishlist: "/assets/images/favorite-fill.svg",
+      },
+      {
+        id: 2,
+        title: "The Overnight Guest",
+        description:
+          "True crime writer Wylie Lark doesn't mind being snowed in at the isolated farmhouse...",
+        img: "/assets/images/top-slider/top3.png",
+        price: "$20.50",
+        cart: "/assets/images/icon.svg",
+        wishlist: "/assets/images/favorite-fill.svg",
+      },
+      {
+        id: 3,
+        title: "The Forever Witness: How...",
+        description:
+          "A relentless detective and a civilian genealogist solve a haunting cold case and launch...",
+        img: "/assets/images/top-slider/top4.png",
+        price: "$20.50",
+        cart: "/assets/images/icon.svg",
+        wishlist: "/assets/images/favorite-fill.svg",
+      },
+      {
+        id: 4,
+        title: "The Hollow of Fear",
+        description:
+          "Moriarty's shadow looms large. First, Charlotte's half brother disappears. Then, Lady Ingram...",
+        img: "/assets/images/top-slider/top5.png",
+        price: "$20.50",
+        cart: "/assets/images/icon.svg",
+        wishlist: "/assets/images/favorite-fill.svg",
+      },
+    ],
+  },
+  recommended: {
+    title: "Recommended for you",
+    items: [
+      {
+        id: 1,
+        title: "Under the Skin: The Hidden..",
+        description: "Under the Skin, Linda Villarosa lays bare the forces in the American health-care system...",
+        img: "/assets/images/recommended-slider/rec3.png",
+        price: "$ 27.99",
         cart: "/assets/images/icon.svg",
         wishlist: "/assets/images/favorite-fill.svg"
       },
       {
         id: 2,
-
+        title: "Shadow and Bone",
+        description:
+          "Orphaned and expendable, Alina Starkov is a soldier who knows she may not survive her first...",
+        img: "/assets/images/recommended-slider/rec4.png",
+        price: "$15.50",
+        cart: "/assets/images/icon.svg",
+        wishlist: "/assets/images/favorite-fill.svg"
+      },
+      {
+        id: 3,
+        title: "The Lost Girls of Willowb...",
+        description: "Six years after Rosemary's death from pneumonia, Sage, now sixteen, still misses her deeply...",
+        img: "/assets/images/recommended-slider/rec5.png",
+        price: "$18.99",
+        cart: "/assets/images/icon.svg",
+        wishlist: "/assets/images/favorite-fill.svg"
+      },
+      {
+        id: 4,
+        title: "The house on mango street",
+        description: "In the spirit of Paolo Bacigalupi and Laini Taylor, this futuristic star-crossed love story...",
+        img: "/assets/images/recommended-slider/rec6.png",
+        price: "$19.99",
+        cart: "/assets/images/icon.svg",
+        wishlist: "/assets/images/favorite-fill.svg"
       }
     ]
   }
-}
+};
 
 export default data;
