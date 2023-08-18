bf03d0b562e15e1610f5bf9f1fb2dd011379364d
Claudiu Sonica <csonica95@gmail.com>
2023-08-18 23:11:22 +0200

    added hover effect for buttons

--- src/components/atoms/button/Button.jsx
+++ src/components/atoms/button/Button.jsx
@@ -1,9 +1,25 @@
 /* eslint-disable react/prop-types */
 import "./Button.scss";
+import cartHov from "/assets/images/cart-hov.svg";
+import { useState } from "react";
 
-const Button = ({text, img, className}) => {
+const Button = ({ text, img, className }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const mouseOn = () => {
+    setIsHovered(prevHover => !prevHover);
+  }
+  const mouseOut = () => {
+    setIsHovered(prevHover => !prevHover);
+  }
   return (
-    <button className={className}>{ img && <img src={img} alt="cart icon" /> }{ text }</button>
+    <button
+      onMouseEnter={mouseOn}
+      onMouseLeave={mouseOut}
+      className={className}>
+      {img && <img src={isHovered ? cartHov : img} alt="cart icon" />}
+      {text}
+    </button>
   );
 };
 
